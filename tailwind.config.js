d962868a51cfbb80cb389190eb8b7fbeff855019
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-12-21 17:51:15 +0800

    fix: fix break points

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: '480px',
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1600px',
+        '2xl': '1600px',
+        xxxl: '2000px',
+        '3xl': '2000px',
+      },
+    },
   },
   corePlugins: {
     preflight: false,
