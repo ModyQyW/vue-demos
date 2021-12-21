ed457818192df0d84972072121b9634ab26c836e
ModyQyW <wurui7@mail3.sysu.edu.cn>
2021-12-21 17:52:08 +0800

    fix: fix lang

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <a-config-provider :locale="zhCN">
+  <a-config-provider>
     <Layout>
       <router-view />
     </Layout>
@@ -7,6 +7,5 @@
 </template>
 
 <script setup lang="ts">
-import zhCN from 'ant-design-vue/es/locale/zh_CN';
 import Layout from '@/layouts/index.vue';
 </script>
